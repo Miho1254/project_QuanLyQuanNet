d352ad57093d8d940e98a96cd70d1e605a43e070
meotrang3051 <dinhhieu3051@gmail.com>
2023-06-01 21:32:56 +0700

    test thử gitadd

--- project_QuanLyQuanNet/Form1.cs
+++ project_QuanLyQuanNet/Form1.cs
@@ -16,5 +16,11 @@ namespace project_QuanLyQuanNet
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // dự án 
+
+        }
     }
 }
