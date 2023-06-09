b835d19548106ca9e203ab01e8ced6981379d896
meotrang3051 <dinhhieu3051@gmail.com>
2023-06-09 10:12:08 +0700

    đăng nhập vào máy người chơi

---
+++ Server/Admin_Login.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Server
+{
+    partial class Admin_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_dangNhap = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.tbx_Username = new System.Windows.Forms.TextBox();
+            this.tbx_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_Dangnhap = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_dangNhap
+            // 
+            this.lbl_dangNhap.AutoSize = true;
+            this.lbl_dangNhap.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_dangNhap.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dangNhap.Location = new System.Drawing.Point(126, 9);
+            this.lbl_dangNhap.Name = "lbl_dangNhap";
+            this.lbl_dangNhap.Size = new System.Drawing.Size(307, 33);
+            this.lbl_dangNhap.TabIndex = 1;
+            this.lbl_dangNhap.Text = "Đăng Nhập Vào Máy ";
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Username.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.Location = new System.Drawing.Point(12, 73);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(154, 33);
+            this.lbl_Username.TabIndex = 1;
+            this.lbl_Username.Text = "Username";
+            // 
+            // tbx_Username
+            // 
+            this.tbx_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_Username.Location = new System.Drawing.Point(174, 73);
+            this.tbx_Username.Multiline = true;
+            this.tbx_Username.Name = "tbx_Username";
+            this.tbx_Username.Size = new System.Drawing.Size(236, 33);
+            this.tbx_Username.TabIndex = 4;
+            // 
+            // tbx_Password
+            // 
+            this.tbx_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_Password.Location = new System.Drawing.Point(174, 126);
+            this.tbx_Password.Multiline = true;
+            this.tbx_Password.Name = "tbx_Password";
+            this.tbx_Password.Size = new System.Drawing.Size(236, 33);
+            this.tbx_Password.TabIndex = 5;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Password.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(12, 126);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(145, 33);
+            this.lbl_Password.TabIndex = 1;
+            this.lbl_Password.Text = "Password";
+            // 
+            // btn_Dangnhap
+            // 
+            this.btn_Dangnhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_Dangnhap.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
+            this.btn_Dangnhap.FlatAppearance.BorderSize = 0;
+            this.btn_Dangnhap.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Fuchsia;
+            this.btn_Dangnhap.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.btn_Dangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Dangnhap.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Dangnhap.ForeColor = System.Drawing.Color.Black;
+            this.btn_Dangnhap.Location = new System.Drawing.Point(174, 183);
+            this.btn_Dangnhap.Name = "btn_Dangnhap";
+            this.btn_Dangnhap.Size = new System.Drawing.Size(166, 42);
+            this.btn_Dangnhap.TabIndex = 6;
+            this.btn_Dangnhap.Text = " Đăng Nhập";
+            this.btn_Dangnhap.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Server.Properties.Resources._313010374_1062211354473854_7487182558124643172_n;
+            this.pictureBox1.Location = new System.Drawing.Point(344, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 206);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Admin_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(526, 256);
+            this.Controls.Add(this.btn_Dangnhap);
+            this.Controls.Add(this.tbx_Password);
+            this.Controls.Add(this.tbx_Username);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.lbl_dangNhap);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Admin_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin_Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_dangNhap;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.TextBox tbx_Username;
+        private System.Windows.Forms.TextBox tbx_Password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_Dangnhap;
+    }
+}
