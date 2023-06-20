b4392e9715e201322e18b84706f5b32e4c233135
Miho1254 <nguyenha280876@gmail.com>
2023-06-20 10:19:56 +0700

    Fix server initalize issue

--- Server/Program.cs
+++ Server/Program.cs
@@ -22,16 +22,36 @@ namespace Server
                 StartSqlServer();
             }
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            // Khởi động socket trong một Task không đồng bộ
+            Task socketTask = Task.Run(() =>
+            {
+                SocketServer socket = new SocketServer();
+                socket.Start(ConfigurationManager.AppSettings["ServerIP"], Convert.ToInt32(ConfigurationManager.AppSettings["ServerPort"]), false);
+                Console.WriteLine("Đã khởi tạo socket");
+            });
 
             // Hiển thị form Admin_Login
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Admin_Login adminLogin = new Admin_Login();
-            adminLogin.Show();
+            Application.Run(adminLogin);
 
+            // Đợi socketTask hoàn thành
+            socketTask.Wait();
 
-            // Chạy vòng lặp sự kiện của ứng dụng
-            Application.Run ();
+            // Khởi tạo máy chủ SQL LocalDB
+            string connectionString = ConfigurationManager.ConnectionStrings["Server.Properties.Settings.CSDL_Server_QuanNetConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Lỗi kết nối tới SQL Server: " + ex.Message);
+                }
+            }
         }
 
         private static bool IsSqlServerRunning()
