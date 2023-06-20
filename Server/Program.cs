7493e88e0c3de4e75267981bb4e53c7c96c0c6f2
Miho1254 <nguyenha280876@gmail.com>
2023-06-20 09:47:51 +0700

    Optimize source code

--- Server/Program.cs
+++ Server/Program.cs
@@ -25,10 +25,6 @@ namespace Server
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Hiển thị form Form1
-            Form1 form1 = new Form1();
-            form1.Show();
-
             // Hiển thị form Admin_Login
             Admin_Login adminLogin = new Admin_Login();
             adminLogin.Show();
